3efd4dbf0918dfd749944b1989ee7b15ba063a18
Luca Lisotti <luca.lisotti@yahoo.com>
2015-10-12 20:21:21 +0200

    La funzione exec non settava la variabile $return_value perche' era posizionata come argomento anziche' come variabile di ritorno. Aggiunta l'opzione per fare il pull anche dei tag. Corretto l'eseguibile git.

--- php/root_functions.php
+++ php/root_functions.php
@@ -239,7 +239,7 @@ else {
         break;
     case "update": //Aggiornamento software
         $mylog->logInfo("Tentativo di aggiornamento software dalla versione ".VERSION);
-        exec(GIT_PATH."git.exe pull origin master", $return_value ); //Per eseguire delle prove aggiungere --dry-run al comando git
+        $return_value=exec(GIT_EXECUTABLE."pull --tags origin master"); //Per eseguire delle prove aggiungere --dry-run al comando git
         if($return_value==0) {
             $mylog->logInfo("Tentativo riuscito: nuova versione: ".VERSION);
             $_SESSION['update']=FALSE;
