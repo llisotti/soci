a218835cea8585ed60af1269d2464ae71c4a8192
Luca Lisotti <luca.lisotti@yahoo.com>
2015-11-22 18:20:48 +0100

    In Debian le sessioni sono cancellate ogni 30 minuti a causa della presenza di un job in cron (/etc/cron.d/php). Per prolungare il tempo della sessione, senza modificare le impostazioni del sistema, il file della sessione ora viene creato nella cartella doc del progetto. In questa cartella il demone cron non agisce.

--- index.php
+++ index.php
@@ -5,6 +5,18 @@ if(PHP_OS=="Linux")
 else
     require "php\member.php";
 
+/* Setto la sessione di 5 ore */
+ini_set('session.gc_maxlifetime', 18000);
+//echo ini_get('session.gc_maxlifetime');
+
+/* Abilito il garbage collector con la probabilita' dell'1% di girare ad ogni session_start() */
+ini_set('session.gc_probability', 1);
+ini_set('session.gc_divisor', 100);
+
+/* Definisco un mio path dove dovra' essere creato il file della sessione */
+session_save_path(LOGFILE_PATH);
+
+/* Inizio sessione */
 session_start();
 
 /**
@@ -19,10 +31,6 @@ session_start();
  * Luca Lisotti
  */
 
-
-/* Setto la sessione di 5 ore */
-ini_set('session.gc_maxlifetime',18000);
-//echo ini_get('session.gc_maxlifetime'); 
 ?>
 <!DOCTYPE html>
 <html>
