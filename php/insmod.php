667f3e46382d7999ed17a7b1784d105ff38ed64f
Luca Lisotti <luca.lisotti@yahoo.com>
2015-12-18 17:57:59 +0100

    Corretto il commit a218835c nel quale mi ero dimenticato di modificare il file insmod.php.

--- php/insmod.php
+++ php/insmod.php
@@ -1,10 +1,21 @@
 <?php
 require "member.php"; //OBBLIGATORIO AVERE IL TEMPLATE DELLA CLASSE PRIMA DELL'INIZIO DELLA SESSIONE  !
-session_start();
 
 /* Setto la sessione di 5 ore */
-ini_set('session.gc_maxlifetime',18000);
-//echo ini_get('session.gc_maxlifetime'); 
+ini_set('session.gc_maxlifetime', 18000);
+//echo ini_get('session.gc_maxlifetime');
+
+/* Abilito il garbage collector con la probabilita' dell'1% di girare ad ogni session_start() */
+ini_set('session.gc_probability', 1);
+ini_set('session.gc_divisor', 100);
+
+/* Definisco un mio path dove dovra' essere creato il file della sessione */
+session_save_path(LOGFILE_PATH);
+
+/* Inizio sessione */
+session_start();
+session_cache_limiter('private,must-revalidate');
+
 ?>
 <!DOCTYPE html>
 <html>
