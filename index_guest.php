d025501fefc63e84a91b991ed1fdbc29b56ca2ca
Luca Lisotti <luca.lisotti@yahoo.com>
2022-11-26 17:40:52 +0100

    Per essere meno invasivo ora anche l'alert sul formato codice fiscale viene emesso solamente una volta.

--- index_guest.php
+++ index_guest.php
@@ -350,7 +350,7 @@ $(document).ready(function(){
 	*/
 
     /* Validazione codice fiscale (lo controllo quando entro nel campo comune) */
-	$("#comune").focus(function() {
+	$("#comune").one('focus',function() {
     	// http://blog.marketto.it/2016/01/regex-validazione-codice-fiscale-con-omocodia/
     	var pattern = /^(?:[A-Z][AEIOU][AEIOUX]|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}(?:[\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[15MR][\dLMNP-V]|[26NS][0-8LMNP-U])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM]|[AC-EHLMPR-T][26NS][9V])|(?:[02468LNQSU][048LQU]|[13579MPRTV][26NS])B[26NS][9V])(?:[A-MZ][1-9MNP-V][\dLMNP-V]{2}|[A-M][0L](?:[1-9MNP-V][\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]$/i;
     	if(!$("#cf").val().match(pattern))
