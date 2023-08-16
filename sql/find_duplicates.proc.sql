82be59dc5aa2cb36d262534348531d64a870aa26
Luca Lisotti <luca.lisotti@yahoo.com>
2023-08-16 21:48:15 +0200

    Modificata la procedura per la ricerca di un'eventuale iscrizione doppia in funzione delle nuove tabelle modificate al commit 9b5dfeda4ff41063cfb695c06e759d5f4fcfb16e per l'eliminazione del codice fiscale

--- sql/find_duplicates.proc.sql
+++ sql/find_duplicates.proc.sql
@@ -1,49 +1,49 @@
 /* Procedura registrata sul db soci */
 USE soci;
-#USE prova;
 DELIMITER $$
 DROP PROCEDURE IF EXISTS find_duplicates;
-CREATE PROCEDURE find_duplicates(IN action BOOLEAN)
+CREATE PROCEDURE find_duplicates()
 BEGIN
-DECLARE surname VARCHAR(30);
-DECLARE name VARCHAR(30);
-DECLARE temp VARCHAR(30);
+DECLARE surname VARCHAR(50);
+DECLARE name VARCHAR(50);
+DECLARE born DATE;
+DECLARE surname_temp VARCHAR(50);
+DECLARE name_temp VARCHAR(50);
+DECLARE born_temp DATE;
+DECLARE duplicato VARCHAR (100);
 DECLARE id_index SMALLINT UNSIGNED DEFAULT 0;
 DECLARE id_scroll SMALLINT UNSIGNED DEFAULT 0;
-DECLARE corretti SMALLINT UNSIGNED DEFAULT 0;
-DECLARE _tessera SMALLINT UNSIGNED DEFAULT 0;
-DECLARE _data DATE;
-SET id_index = (SELECT MAX(member_id) FROM anagrafica); /* Trovo il member_id più alto */
-SET id_scroll = id_index-1;
+SET id_index = (SELECT MAX(id) FROM anagrafica); /* Trovo l'id più alto */
+SET id_scroll = id_index-1; /* Parto a scrollare dall'id immediatamente inferiore */
+/* Se l'id_index non esiste decremento id_index e naturalmente id_scroll altrimenti ... */
 label:WHILE (id_index != 1) DO
-IF NOT EXISTS(SELECT member_id FROM anagrafica WHERE member_id=id_index) THEN /* Se il member_id non esiste decremento member_id e itero di nuovo altrimenti ... */
+IF NOT EXISTS(SELECT id FROM anagrafica WHERE id=id_index) THEN
 SET id_index = id_index-1;
 SET id_scroll = id_index-1;
 ITERATE label;
 END IF;
-SELECT cognome FROM anagrafica WHERE member_id=id_index INTO surname; /* ... seleziono il cognome */
+/* ... seleziono il cognome, il nome e la data di nascita di id_index */
+SELECT cognome FROM anagrafica WHERE id=id_index INTO surname;
+SELECT nome FROM anagrafica WHERE id=id_index INTO name;
+SELECT data_nascita FROM anagrafica WHERE id=id_index INTO born;
+/* Se l'id_scroll non esiste decremento id_scroll e riprovo altrimenti ... */
 label_2:WHILE (id_scroll != 1) DO
-IF NOT EXISTS(SELECT member_id FROM anagrafica WHERE member_id=id_scroll AND tessera IS NULL) THEN /* Se il member_id senza tessera non esiste decremento member_id e riprovo altrimenti ... */
+IF NOT EXISTS(SELECT id FROM anagrafica WHERE id=id_scroll) THEN
 SET id_scroll = id_scroll-1;
 ITERATE label_2;
 END IF;
-SELECT cognome FROM anagrafica WHERE member_id=id_scroll  INTO temp; /* ... seleziono il cognome */
-IF (temp=surname) THEN /* Se i due member_id hanno lo stesso cognome controllo i due nomi */
-SELECT nome FROM anagrafica WHERE member_id=id_index INTO name;
-SELECT nome FROM anagrafica WHERE member_id=id_scroll INTO temp;
-IF (temp=name) THEN /* Se i due member_id hanno lo stesso nome allora è un duplicato */
-SET corretti=corretti+1; 
-IF (action) THEN /* Se ho chiamato la procedura con TRUE allora applico la correzione */
-SELECT tessera FROM anagrafica WHERE member_id=id_index INTO _tessera;
-SELECT data FROM presenze WHERE member_id=id_index INTO _data;
-UPDATE anagrafica SET tessera=_tessera WHERE member_id=id_scroll;
-UPDATE presenze SET data=_data WHERE member_id=id_scroll;
-DELETE FROM anagrafica WHERE member_id=id_index;
-DELETE FROM presenze WHERE member_id=id_index;
-ELSE
-SELECT member_id, cognome, nome FROM anagrafica WHERE member_id=id_index;
-SELECT member_id, cognome, nome FROM anagrafica WHERE member_id=id_scroll;
-SELECT '*****************************************';
+/* ... seleziono il cognome di id_scroll e lo confronto con quello di id_index */
+SELECT cognome FROM anagrafica WHERE id=id_scroll INTO surname_temp;
+IF (STRCMP(surname, surname_temp) = 0) THEN
+/* Se sono uguali seleziono il nome di id_scroll e lo confronto con quello di id_index */
+SELECT nome FROM anagrafica WHERE id=id_scroll INTO name_temp;
+IF (STRCMP(name, name_temp) = 0) THEN
+/* Se sono uguali seleziono la data di nascita di id_scroll e la confronto con quella di id_index */
+SELECT data_nascita FROM anagrafica WHERE id=id_scroll INTO born_temp;
+IF (born_temp = born) THEN
+/* Se anche le date di nascita sono uguali allora è un doppio! */
+SELECT CONCAT(surname, " ", name) INTO duplicato;
+SELECT duplicato;
 END IF;
 END IF;
 END IF;
@@ -52,13 +52,5 @@ END WHILE;
 SET id_index = id_index-1;
 SET id_scroll = id_index-1;
 END WHILE;
-IF (corretti=0) THEN
-SELECT 'Nessuna' AS 'Identita\' duplicate';
-ELSEIF(!action) THEN
-#SELECT CONCAT(corretti, ' identita\' duplicate') AS '';
-SELECT corretti AS 'Identita\' duplicate';
-ELSE
-SELECT corretti AS 'Identita\' corrette';
-END IF;
 END $$
 DELIMITER ;
